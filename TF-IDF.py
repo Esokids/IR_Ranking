25f194e495c9c4e2122e1c93448b2123cd9fd471
Winz <mawin_07@hotmail.com>
2019-05-16 17:44:26 +0700

    Wildcard

--- TF-IDF.py
+++ TF-IDF.py
@@ -1,6 +1,7 @@
 import glob
 import pandas as pd
 import re
+import fnmatch
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.stem import SnowballStemmer
 from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
@@ -25,16 +26,29 @@ def tokenize(text):
     return stems
 
 
-def search(keyword=None, data=None):
-    if keyword is None:
+def search_preprocess(keyword, df):
+    tokens = df.columns
+    keyword = keyword.split()
+    search_words = list()
+
+    if len(keyword) == 0:
+        return None
+    else:
+        for i in keyword:
+            search_words.extend(fnmatch.filter(tokens, i))
+
+    return search_words
+
+
+def search(keyword=None, df=None):
+    if len(keyword) == 0:
         return False
 
     temp = pd.DataFrame()
-
     for i in keyword:
-        if i in data:
-            temp[i] = pd.Series(data[i].values, index=data.index)
-    temp['Sum'] = pd.Series(temp.sum(axis=1), index=data.index)
+        if i in df:
+            temp[i] = pd.Series(df[i].values, index=df.index)
+    temp['Sum'] = pd.Series(temp.sum(axis=1), index=df.index)
     temp = temp.sort_values(by=['Sum'], ascending=False)
 
     idx = temp.index[temp['Sum'] > 0].tolist()
@@ -49,11 +63,15 @@ def main():
     x = v.fit_transform(df['text'])
 
     tfidf = pd.DataFrame(x.toarray(), columns=v.get_feature_names())
-    #print(tfidf)
+    search_words = search_preprocess('150 y*', tfidf)
+    print(search_words)
+    result = search(search_words, tfidf)
 
-    result = search(tokenize('150 year'), tfidf)
-    for i in result:
-        print(f"{int(i)+1}.txt")
+    if result is False:
+        print("Not found")
+    else:
+        for i in result:
+            print(f"{int(i)+1}.txt")
 
 
 if __name__ == '__main__':
